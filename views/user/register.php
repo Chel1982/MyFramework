c083e16daa9d6abee2df74ddd8d24c6e5b8d6334
andrew <chel@activist.com>
2016-08-26 11:02:20 +0300

    Mi first framework

--- views/user/register.php
+++ views/user/register.php
@@ -6,13 +6,16 @@
 
             <div class="col-sm-4 col-sm-offset-4 padding-right">
 
-                <?php if (isset($errors) && is_array($errors)): ?>
-                    <ul>
-                        <?php foreach ($errors as $error): ?>
-                            <li> - <?= $error ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
+                <?php if ($result): ?>
+                    <p>Вы зарегестрированы!</p>
+                <?php else: ?>
+                    <?php if (isset($errors) && is_array($errors)): ?>
+                        <ul>
+                            <?php foreach ($errors as $error): ?>
+                                <li> - <?= $error ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
                         <p>Регистрация на сайте</p>
                     <div class="signup-form"><!--sign up form-->
                         <h2>Регистрация на сайте</h2>
@@ -23,6 +26,7 @@
                             <input type="submit" name="submit" class="btn btn-default" value="Регистрация" />
                         </form>
                     </div><!--/sign up form-->
+                <?php endif; ?>
                 <br/>
                 <br/>
             </div>
